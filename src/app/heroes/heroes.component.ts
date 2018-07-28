bdefb70ea0ae5a82e4bc51742f89da04f9305a03
leilai <leilai@ajou.ac.kr>
2018-07-28 17:07:15 +0900

    #004 부트랩 설치 및 스타일링

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -9,20 +9,28 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   isSpecial = false;
+
   hero: Hero = {
     id: 1,
     name: 'Winstorm'
   };
 
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.selectedHero);
   }
 
   onSave(event: any) {
     console.log(event);
     console.log(this.isSpecial);
+  }
+
+  onSelected(hero: Hero) {
+    console.log(hero);
+    this.selectedHero = hero;
   }
 }
